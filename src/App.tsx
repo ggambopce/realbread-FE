cce7a7a50c1c6ba03567c14196d97021241be3c4
ggambopce <ggambopce@gmail.com>
2025-03-27 09:41:21 +0900

    🎉 리얼빵집FE 시작

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import NaverMap from './components/NaverMap'
+
+function App() {
+
+  
+
+  return (
+    <>
+      
+      <h1>리얼 맛집</h1>
+      <NaverMap />
+      
+      
+    </>
+  )
+}
+
+export default App
