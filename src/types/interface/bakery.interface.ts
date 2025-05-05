76f4a97cf76b353e73f62dba2d8c5c2800156de5
ggambopce <ggambopce@gmail.com>
2025-05-05 19:56:05 +0900

    ✨ 상세페이지 인터페이스

---
+++ src/types/interface/bakery.interface.ts
@@ -0,0 +1,10 @@
+import MenuListItem from "./menu-list-item.interface";
+
+export default interface Bakery {
+    bakeryNumber: number;
+    title: string;
+    roadAddress: string;
+    favoriteCount: number;
+    commentCount: number;
+    munuList: MenuListItem[];
+}
---
+++ src/types/interface/menu-list-item.interface.ts
@@ -0,0 +1,7 @@
+export default interface MenuListItem {
+    menuNumber: number;
+    menuName: string;
+    price: string;
+    description: string;
+    imageUrl: string;
+}
