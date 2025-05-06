d57d6ebbb14a8ff4251b06087d70d4e47c674ec6
ggambopce <ggambopce@gmail.com>
2025-05-06 16:44:46 +0900

    ✨ 좋아요 리스트 컴포넌트

---
+++ src/mocks/favorite-list.mock.ts
@@ -0,0 +1,22 @@
+import { FavoriteListItem } from "types/interface";
+
+const favoriteListMock: FavoriteListItem[] = [
+    {
+        email: "email@email.com",
+        nickname: "안녕 나는 JINO",
+        profileImage: 'https://fastly.picsum.photos/id/20/200/200.jpg?hmac=wHmtG3BEC6aOsGZU_Q2wnxVQq34B__t4x4LFw-sptM8',
+    },
+    {
+        email: "email@email.com",
+        nickname: "안녕 나는 JINO",
+        profileImage: 'https://fastly.picsum.photos/id/20/200/200.jpg?hmac=wHmtG3BEC6aOsGZU_Q2wnxVQq34B__t4x4LFw-sptM8',
+    },
+    {
+        email: "email@email.com",
+        nickname: "안녕 나는 JINO",
+        profileImage: 'https://fastly.picsum.photos/id/20/200/200.jpg?hmac=wHmtG3BEC6aOsGZU_Q2wnxVQq34B__t4x4LFw-sptM8',
+    },
+
+]
+
+export default favoriteListMock
