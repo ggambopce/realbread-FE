bd68262e88d8efab61333426e1f097bbb4c57d8b
ggambopce <ggambopce@gmail.com>
2025-05-21 15:55:23 +0900

    ✨ 검색 컴포넌트 수정

--- src/components/searchBox/index.tsx
+++ src/components/searchBox/index.tsx
@@ -6,8 +6,6 @@ export default function SearchButton() {
   
     //          state: 검색어 버튼 요소 참소 상태           //
     const searchButtonRef = useRef<HTMLDivElement | null>(null);
-    //          state: 검색 버튼 상태           //
-    const [status, setStatus] = useState<boolean>(false);
     //          state: 검색어 상태          //
     const [word, setWord] = useState<string>('');
     //          state: 검색어 path variable 상태          //
@@ -29,12 +27,7 @@ export default function SearchButton() {
     }
     //          event handler: 검색 버튼 클릭 이벤트 처리 함수          //
     const onSearchButtonClickHandler = () => {
-        if(!status) {
-            setStatus(!status);
-            return;
-        }
         if (!word) return;
-
         // 검색어를 URL에 반영
         navigator(`/main/search/${word}`);
     }
@@ -42,18 +35,11 @@ export default function SearchButton() {
     //          effect: 검색어 path variable 변경될 때마다 실행된 함수         //
     useEffect(() => {
         if (searchWord) {
-        setWord(searchWord);
-        setStatus(true);
+            setWord(searchWord);
         }
     }, [searchWord])
     
-    if (!status)
-    //          render: 검색 버튼 컴포넌트 랜더링 (클릭 false 상태)           //
-    return (
-        <div className='icon-button' onClick={onSearchButtonClickHandler}>
-            <div className='icon search-light-icon'></div>
-        </div>
-    );
+
     //          render: 검색 버튼 컴포넌트 랜더링 (클릭 true 상태)           //
     return (
         <div className='search-input-box'>
